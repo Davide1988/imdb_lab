4cda61377aedbc81d80fc06129a2afad71a51b9d
Michael Irwin <mike.irwin1@gmail.com>
2018-12-06 15:49:35 +0000

    extensions complete

--- console.rb
+++ console.rb
@@ -11,10 +11,10 @@ star2 = Star.new({'first_name' => 'Jennifer', 'last_name' => 'Lawrence'})
 star1.save
 star2.save
 
-movie1 = Movie.new({'title' => 'Guardians', 'genre' => 'Superhero'})
-movie2 = Movie.new({'title' => 'Passenger', 'genre' => 'Sci-fi'})
-movie3 = Movie.new({'title' => 'The Hunger Games', 'genre' => 'Sci-fi'})
-movie4 = Movie.new({'title' => 'Jurassic World', 'genre' => 'Sci-fi'})
+movie1 = Movie.new({'title' => 'Guardians', 'genre' => 'Superhero', 'budget' => 30})
+movie2 = Movie.new({'title' => 'Passenger', 'genre' => 'Sci-fi', 'budget' => 40})
+movie3 = Movie.new({'title' => 'The Hunger Games', 'genre' => 'Sci-fi', 'budget' => 10})
+movie4 = Movie.new({'title' => 'Jurassic World', 'genre' => 'Sci-fi', 'budget' => 10})
 movie1.save
 movie2.save
 movie3.save
@@ -27,5 +27,6 @@ casting2.save
 casting3 = Casting.new({'movie_id' => movie2.id, 'star_id' => star2.id, 'fee' => 4})
 casting3.save
 guardians_stars = movie1.stars
+p movie2.budget_left
 binding.pry
 nil
--- models/movie.rb
+++ models/movie.rb
@@ -4,20 +4,21 @@ require_relative('casting')
 
 class Movie
 
-  attr_accessor :title, :genre
+  attr_accessor :title, :genre, :budget
   attr_reader :id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @title = options['title']
     @genre = options['genre']
+    @budget = options['budget']
   end
 
   def save()
-    sql = "INSERT INTO movies (title, genre)
-    VALUES ($1,$2)
+    sql = "INSERT INTO movies (title, genre, budget)
+    VALUES ($1,$2, $3)
     RETURNING id"
-    values = [@title, @genre]
+    values = [@title, @genre, @budget]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
@@ -34,5 +35,15 @@ class Movie
     values = [@id]
     star_hashes = SqlRunner.run(sql, values)
     return star_hashes.map {|star| Star.new(star)}
+  end
+
+  def budget_left()
+    sql = "SELECT SUM(casting.fee) FROM
+    casting INNER JOIN movies
+    ON movies.id = casting.movie_id
+    WHERE movies.id = $1"
+    values = [@id]
+    total_fees = SqlRunner.run(sql, values).first
+    return @budget - total_fees['sum'].to_i
   end
 end
